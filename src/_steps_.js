befd00068ab1a26b13979044e4f1c6ac9d2b0afd
shawon012 <shawon.shakib01@gmail.com>
2023-04-16 14:21:04 +0600

    first commit

---
+++ src/_steps_.js
@@ -0,0 +1,9 @@
+/***
+ * 1. authentication 
+ * 2. authorization
+ * 3. sign in == login 
+ * 4. signup === register
+ * 5.
+ * 
+ * 
+ * */ 
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h2>Register</h2>
+            <form>
+                <input type="email" name="email" id="email" placeholder='Your Email' />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
+
+
+// echo "# email-password-auth" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/shawon012/email-password-auth.git
+// git push -u origin main
