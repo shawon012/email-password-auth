a62c31e0ef1f1fe642d8b9f6badd07dc5352031c
shawon012 <shawon.shakib01@gmail.com>
2023-04-16 15:51:01 +0600

    react-bootsrap is not installed yet

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(email, password)
+    }
+
+    const handleEmailChange = (event) => {
+        // console.log(event.target.value)
+        setEmail(event.target.value)
+    }
+    const handlePasswordBlur = (event) =>{
+        // console.log(event.target.value)
+    }
     return (
         <div>
-            <h2>Register</h2>
-            <form>
-                <input type="email" name="email" id="email" placeholder='Your Email' />
+            <h2>Please Register</h2>
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
+                <br />
+                <input onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
+                <br />
+                <input type="submit" value="Register"  />
             </form>
         </div>
     );
