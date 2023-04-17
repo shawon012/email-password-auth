6bf5ac2135ded3b99736ca7450ee026078ac78f4
shawon012 <shawon.shakib01@gmail.com>
2023-04-17 16:15:22 +0600

    email error and succesfull set

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,27 +5,51 @@ import app from '../../firebase/firebase.config';
 const auth = getAuth(app);
 
 const Register = () => {
-    const [email, setEmail] = useState('');
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    // const [email, setEmail] = useState('');
 
     const handleSubmit = (event) => {
+        // prevent page reload
         event.preventDefault();
+        setSuccess('')
+        setError('')
+        // collect form data
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password)
+        // validate
+        if(!/.*[A-Z]/.test(password)){
+            setError('Please add atleast one uppercase');
+            return
+        }
+        else if(!/.*[0-9].*[0-9]/.test(password)){
+            setError('Please add at least two numbers')
+            return
+        }
+        else if(password.length<6){
+            setError('please add atleast 6 character')
+            return
+        }
         // create user in firebase
         createUserWithEmailAndPassword(auth, email, password)
         .then( result =>{
             const loggedUser = result.user
             console.log(loggedUser)
+            setError('');
+            event.target.reset()
+            setSuccess('User has been created successfully')
         })
         .catch(error => {
-            console.log(error);
+            console.error(error.message)
+            setError(error.message)
+            setSuccess('')
         })
     }
 
     const handleEmailChange = (event) => {
         // console.log(event.target.value)
-        setEmail(event.target.value)
+        // setEmail(event.target.value)
     }
     const handlePasswordBlur = (event) =>{
         // console.log(event.target.value)
@@ -34,12 +58,16 @@ const Register = () => {
         <div className='w-50 mx-auto'>
             <h2>Please Register</h2>
             <form onSubmit={handleSubmit}>
-                <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
+                <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' required />
                 <br />
-                <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
+                <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Your Password' required />
                 <br />
+                <p className='text-danger'>{error}</p>
+                <p className='text-success'>{success}</p>
+                {/* <br /> */}
                 <input className='btn btn-primary' type="submit" value="Register"  />
             </form>
+            
         </div>
     );
 };
